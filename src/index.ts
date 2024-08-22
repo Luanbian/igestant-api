378ad3ec2fe3e4cb8dff0aba79e4266286ecd6a8
Luan Almeida <luan.almeida@jbtec.com.br>
2024-08-21 21:46:20 -0300

    feat: health chack route

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import debug from 'debug';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+import './services';
+// import { app } from './services/express';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+const logger = debug('core');
+
+logger('Starting API');
---
+++ src/services/express/index.ts
@@ -0,0 +1,38 @@
+import debug from 'debug';
+import express from 'express';
+import cors from 'cors';
+import os from 'os';
+import dayjs from 'dayjs';
+import { v6 } from 'uuid';
+import pkgJson from '../../../package.json';
+import { APIEcho, APIResponse } from './types';
+import { EXPRESS_PORT } from '../../constants/express';
+
+const logger = debug('services:express');
+
+const app = express();
+
+app.set('trust proxy', true);
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (_req, res) => {
+    res.status(200).json({
+        code: 'echo',
+        transaction: v6(),
+        message: 'Ok',
+        args: [],
+        data: {
+            server: os.hostname(),
+            time: dayjs().toISOString(),
+            version: pkgJson.version,
+        },
+    } as APIResponse<APIEcho>);
+});
+
+app.listen(EXPRESS_PORT, () => {
+    logger(`Server started on port ${EXPRESS_PORT}`);
+});
+
+export { app };
