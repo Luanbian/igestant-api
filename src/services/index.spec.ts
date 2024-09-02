efa6297962020621d8310131761deed9403e88b5
Luan Almeida <luan.almeida@jbtec.com.br>
2024-09-01 22:08:38 -0300

    fix: close server after test

--- src/services/index.spec.ts
+++ src/services/index.spec.ts
@@ -1,6 +1,10 @@
 import * as expressModule from '.';
+import { server } from './index';
 
 describe('services index', () => {
+    afterEach((done) => {
+        server.close(done);
+    });
     it('should export express', () => {
         expect(expressModule).toBeDefined();
     });
