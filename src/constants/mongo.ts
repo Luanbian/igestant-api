6fa0789e3879d0f0301a0f90189f23b8d4b863a5
Luan Almeida <luan.almeida@jbtec.com.br>
2024-09-13 22:02:36 -0300

    fat: install mongodb

---
+++ src/constants/mongo.ts
@@ -0,0 +1,3 @@
+export const MONGO_URL =
+    process.env.MONGO_URL || 'mongodb://root:pass@127.0.0.1/platform';
+export const MONGO_DB = process.env.MONGO_DB || 'platform';
---
+++ src/services/mongo/index.ts
@@ -0,0 +1,34 @@
+import debug from 'debug';
+import { Db, MongoClient } from 'mongodb';
+import { MONGO_DB, MONGO_URL } from '../../constants/mongo';
+
+const logger = debug('services:mongo');
+const client = new MongoClient(MONGO_URL);
+
+const status: {
+    db: Db | null;
+} = {
+    db: null,
+};
+
+const connect = async () => {
+    try {
+        logger(`Connecting to MongoDB in: ${MONGO_URL}`);
+        await client.connect();
+        status.db = client.db(MONGO_DB);
+        logger('Connected to MongoDB');
+    } catch (err) {
+        logger('Error connecting to MongoDB: %0', err);
+    }
+};
+
+connect();
+
+const getDb = () => {
+    if (!status.db) {
+        throw new Error('MongoDB not connected');
+    }
+    return status.db;
+};
+
+export { getDb, client, connect };
