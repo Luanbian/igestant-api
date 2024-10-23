57c404f3812bb223eed83cfce8a34b8fee767373
Luan Almeida <luan.almeida@jbtec.com.br>
2024-10-23 19:19:54 -0300

    feat: test the validation middleware

---
+++ src/features/questionAndAnswer/tests/controller/rules.test.ts
@@ -0,0 +1,30 @@
+import { Server } from 'http';
+import request from 'supertest';
+import { app } from '../../../../index';
+import { questionAndAnswersMock } from '../mocks/questionAndAnswerMock';
+
+describe('QuestionAndAnswer Controller Rules Validation', () => {
+    let server: Server;
+    beforeAll((done) => {
+        server = app.listen(0, () => {
+            done();
+        });
+    });
+    afterAll((done) => {
+        server.close(done);
+    });
+
+    it('should return 400 status code, and validation error message in APIResponse format', async () => {
+        const wrongBody = {
+            ...questionAndAnswersMock[0],
+            question: null, // question is required
+        };
+
+        const response = await request(app)
+            .post('/questionAndAnswer')
+            .send(wrongBody);
+
+        // TODO: validate the response body
+        expect(response.status).toBe(400);
+    });
+});
